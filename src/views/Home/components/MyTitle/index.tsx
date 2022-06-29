d98682c6356cce0080d81462aedaf48b083ec6aa
tutu <1515112287@qq.com>
2022-06-29 11:10:57 +0800

    update

--- src/views/Home/components/MyTitle/index.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-const MyTitle = ({ children }: PropsWithChildren<{}>) => {
-  return <div className="text-am26 font-bold  text-left mt-am64 mb-2 ">{children}</div>
-}
-
-export default MyTitle
