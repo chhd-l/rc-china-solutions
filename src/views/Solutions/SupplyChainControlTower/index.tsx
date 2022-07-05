0b9e10c594ec65d1deff4d1afa1277c48005ebc1
chhd <xscnwan13@163.com>
2022-07-05 09:22:36 +0800

    SupplyChainControlTower页面

--- src/views/Solutions/SupplyChainControlTower/index.tsx
+++ src/views/Solutions/SupplyChainControlTower/index.tsx
@@ -1,20 +1,127 @@
+import { NectarButton } from '../../../components/common/NectarButton'
+import SupplyChainControlTowerLogo from '../../../assets/image/SupplyChainControlTower_logo@2x.png'
+import SupplyChainControlTower_bg_01 from '../../../assets/image/SupplyChainControlTower_bg_01.png'
+import SupplyChainControlTower_bg_02 from '../../../assets/image/SupplyChainControlTower_bg_02.png'
+import SupplyChainControlTower_Resources from '../../../assets/image/SupplyChainControlTower_Resources.png'
 import './index.less'
+import { section_03_content, Topreasons_content } from '../modules/SupplyChainControlTower'
 
 const SupplyChainControlTower = () => {
   return (
     <div className="SupplyChainControlTower">
-      <header>
+      <header className="pb-rc132 pl-rc90 pr-rc90">
         <div className="flex items-center justify-between text-rc14 border-b border-gray-400 border-solid pb-rc14">
           <div className="text-rc28 font-bold">Supply Chain Control Tower Software</div>
           <div>
-            <span className='cursor-pointer'>Supply Chain Control Tower</span> —
+            <span className="cursor-pointer">Supply Chain Control Tower</span> —
             <span className="text-black font-bold"> Supply Chain Control Tower</span>
           </div>
         </div>
-        <div className='text-rc66' style={{maxWidth: '900px', marginTop: '180px'}}>
-          <span className='font-bold text-black  leading-rc80'>Real time visibility</span><br /> & automated response at scale
+        <div className="text-rc66 leading-rc1_2 headerTitle_1">
+          <span className="font-bold text-black">Real time visibility</span>
+          <br /> & automated response at scale
+        </div>
+        <div className="mt-rc18 font-bold text-rc16 cursor-pointer">
+          <span>——</span>
+          <span className="ml-rc10">Why choose o9?</span>
+          <NectarButton className="ml-rc25" color="#000" hoverBorderColor="red">
+            Talk to an expert
+          </NectarButton>
+        </div>
+        <div>
+          <img className="mt-rc65" src={SupplyChainControlTowerLogo} alt="" />
+          <div className="border border-solid mt-rc15 mb-rc10 dividon_01" />
+          <div className="text-rc14" style={{ color: '#999' }}>
+            Trusted by leaders across industry verticals.
+          </div>
         </div>
       </header>
+      <main>
+        <section className="bg-black text-white pl-rc90 pr-rc90 section_01">
+          <div className=" pl-rc_15 pr-rc_15 ml-_rc15 mr-_rc15">
+            <div className="pl-rc35 pr-rc35">
+              <div className="relative bg_1">
+                <img src={SupplyChainControlTower_bg_01} alt="" />
+              </div>
+              <div className="text-rc45 mt-rc40 mb-rc40">
+                Sense demand and supply changes across the network and respond in real time with optimal, automated
+                decisions.
+              </div>
+              <div className="text-rc20">
+                The o9 Supply Chain Control Tower leveraging its Digital “Supply Chain Knowledge Graph”, monitors demand
+                and supply signals in real time, detects problems early and drives automated, demand and supply shaping
+                decisions with intelligent evaluation of resolution options.
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="pl-rc90 pr-rc90 relative section_02">
+          <div className="bg-red-300 absolute m-auto bg_2">
+            <img src={SupplyChainControlTower_bg_02} alt="" />
+          </div>
+          <div className="flex pl-rc_8 pr-rc_8 text-rc17">
+            <div className="pl-rc35 pr-rc35">
+              <div className="text-rc35">AI Powered Supply Chain Control Tower</div>
+              <div>
+                Disruptions and market volatility are increasing exponentially. Proactively manage risk and
+                opportunities by sensing and managing demand and supply on an AI platform.
+              </div>
+            </div>
+            <div className="border-t border-solid pt-rc20 dividon_02">
+              More and more data is becoming available. Real-time signals from supply chain disruptions and market
+              opportunities should alert planners instantly. Leveraging the o9 AI scenario capabilities, solutions to
+              these disruptions are presented and highlighted with lead-time and cost implications. The o9 Control Tower
+              allows enterprises to connect and harmonize planning and execution seamlessly on a common AI platform.
+            </div>
+          </div>
+        </section>
+        <section className="section_03 text-white">
+          <div className="bg-black">
+            <div className="flex justify-between Resources mb-rc24">
+              <div>
+                <div className="flex items-center text-rc18 mb-rc30">
+                  <span className="w-rc8 h-rc8 bg-blue-700 mr-rc15" /> Resources
+                </div>
+                <div className="text-rc35 leading-rc42">
+                  View our related <span className="font-bold">white papers and use cases.</span>
+                </div>
+              </div>
+              <div className="flex-1 flex">
+                {section_03_content.map((item, idx) => (
+                  <div
+                    key={idx}
+                    className={`${idx === 1 && 'ml-rc10 mr-rc10'} bg-gray-2a2a2a h-rc300 flex flex-col justify-between`}
+                  >
+                    <img src={item.img} alt="" />
+                    <div className="flex-1 p-rc20 text-rc16">{item.span}</div>
+                    <div className="ResourceBtn text-rc14 flex items-center cursor-pointer">
+                      <img className="w-rc11 mr-rc5" src={SupplyChainControlTower_Resources} alt="" /> View Resource
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="flex justify-end items-center">
+              <div className="text-rc16 pb-rc4 cursor-pointer Allrelatedresources">All related resources</div>
+            </div>
+          </div>
+        </section>
+        <section className="section_04">
+          <div className='flex items-center h-rc18'>
+            <div className='text-rc22 h-rc_110 pr-rc20 tracking-rc7'>Top reasons</div>
+            <div className='flex-1 h-rc1 divider' />
+          </div>
+          <div className='text-rc35 mt-rc20 mb-rc45 tracking-rc5'>Why customers choose o9</div>
+          <div className='flex flex-wrap justify-between'>
+            {
+              Topreasons_content.map((item, idx) => <div className='w-rc_30' key={idx}>
+                <div />
+                <p>{item.span1}{item.strong}{item.span2}{item?.span3}{item?.strong2}</p>
+              </div>)
+            }
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
---
+++ src/views/Solutions/modules/SupplyChainControlTower.ts
@@ -0,0 +1,60 @@
+import section_03_01 from '../../../assets/image/section_03_bg_01@2x.png'
+import section_03_02 from '../../../assets/image/section_03_bg_02@2x.png'
+import section_03_03 from '../../../assets/image/section_03_bg_03@2x.png'
+
+export const section_03_content = [
+  {
+    img: section_03_01,
+    span: 'White Paper: How CPG companies gain control from chaos with Control Towers',
+  },
+  {
+    img: section_03_02,
+    span: 'White Paper: Why Industrial Manufacturers need a Supply Chain Control Tower',
+  },
+  {
+    img: section_03_03,
+    span: 'White Paper: Why Digital Transformation Is a Must for Grocery Retailers',
+  },
+]
+
+export const Topreasons_content = [
+  {
+    icon: '',
+    span1: 'AI enabled ',
+    strong: 'real-time visibility',
+    span2: ' to risks and opportunities with ongoing machine learning and prescriptive recommendations.',
+  },
+  {
+    icon: '',
+    span1: 'Ability to connect to many (unstructured) internal and external ',
+    strong: 'data sources.',
+    span2: '',
+  },
+  {
+    icon: '',
+    span1: 'Ability to ',
+    strong: 'shape demand in real-time',
+    span2: '. Present field opportunities and execute real time D/S matching.',
+  },
+  {
+    icon: '',
+    span1: '',
+    strong: 'Demand alerts',
+    span2:
+      ' (demand surge scenarios, orders exceeding forecasts, orders below forecasts, large changes to orders, etc.), scheduled receipt alerts (shipments arriving later due to bad weather or port delays), procurement alerts and production alerts.',
+  },
+  {
+    icon: '',
+    span1: 'Real-time ',
+    strong: 'IoT sensor alerting',
+    span2: ' for monitoring manufacturing resources (Maintenance, Fault alerts, Energy Consumption,etc)',
+  },
+  {
+    icon: '',
+    span1: 'Seamlessly ',
+    strong: 'connect',
+    span2: ' short-term S&OE planning to ',
+    strong2: 'longer term IBP',
+    span3: ' on a common platform.'
+  },
+]
