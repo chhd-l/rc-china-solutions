872d868d7c9f149b3781a8773defd86b75d14fec
tutu <1515112287@qq.com>
2022-07-08 15:30:37 +0800

    white paper search

--- src/views/Resources/WhitePapers/components/SearchAndFilter/index.tsx
+++ src/views/Resources/WhitePapers/components/SearchAndFilter/index.tsx
@@ -1,6 +1,7 @@
 import { Col, Row } from 'antd'
 import { SearchInput } from '../SearchInput'
 import { Wrapper } from '../Wrapper'
+import { searchItem } from './modules'
 
 const SearchAndFilter = () => {
   return (
@@ -10,6 +11,17 @@ const SearchAndFilter = () => {
           <p className=" text-rc18">Search Resources</p>
           <SearchInput placeholder="Type to search ..." />
         </Wrapper>
+        {searchItem.map((item) => (
+          <Wrapper key={item.title}>
+            <p className=" text-rc18">{item.title}</p>
+            {item.list.map((child) => (
+              <p key={child.label}>
+                {child.label}
+                <span>({child.count})</span>
+              </p>
+            ))}
+          </Wrapper>
+        ))}
       </Col>
       <Col xs={{ span: 18 }} sm={{ span: 16 }} md={{ span: 11 }} lg={{ span: 11 }} xl={{ span: 16 }}></Col>
     </Row>
--- src/views/Resources/WhitePapers/components/SearchAndFilter/modules.ts
+++ src/views/Resources/WhitePapers/components/SearchAndFilter/modules.ts
@@ -1,1 +1,59 @@
-
+export const searchItem = [
+  {
+    title: 'Resource Type',
+    list: [
+      {
+        label: 'Use case ',
+        count: 1,
+      },
+      {
+        label: 'White paper',
+        count: 4,
+      },
+    ],
+  },
+  {
+    title: 'Industries',
+    list: [
+      {
+        label: 'Brand Manufacturing',
+        count: 1,
+      },
+      {
+        label: 'Industrial Manufacturing',
+        count: 2,
+      },
+      {
+        label: 'Retail',
+        count: 5,
+      },
+    ],
+  },
+  {
+    title: 'Solutions',
+    list: [
+      {
+        label: 'Control Tower',
+        count: 3,
+      },
+      {
+        label: 'Demand Planning',
+        count: 1,
+      },
+      {
+        label: 'IBP',
+        count: 1,
+      },
+      {
+        label: 'S&OP',
+        count: 1,
+      },
+    ],
+  },
+]
+export const searchContent = [
+  {
+    img: '',
+    desc: '',
+  },
+]
