0ff9e1cca4bca9d584a97b93e3000daeb9b69cbe
tutu <1515112287@qq.com>
2022-07-13 15:55:58 +0800

    bug fixed

--- src/views/Resources/Events/index.tsx
+++ src/views/Resources/Events/index.tsx
@@ -98,10 +98,34 @@ const Events = () => {
                 </Col>
               </Row>
               <Row justify="center" className="bg-111111">
-                <Col xs={{ span: 18 }} sm={{ span: 16 }} md={{ span: 11 }} lg={{ span: 11 }} xl={{ span: 18 }}>
-                  <p className="text-rc45 text-gray-999999 text-center mt-rc_15 ">
+                <Col xs={{ span: 18 }} sm={{ span: 16 }} md={{ span: 11 }} lg={{ span: 11 }} xl={{ span: 14 }}>
+                  <p className="text-rc45 text-gray-999999  mt-rc_15 ">
                     <span className="text-white">Get in touch</span> with our event specialists
                   </p>
+                  <div className="flex flex-row pb-rc_15">
+                    <div className="text-white">
+                      <img
+                        src="https://o9solutions.com/wp-content/uploads/2020/10/isabella-brandt.jpg"
+                        alt=""
+                        width={258}
+                        height={258}
+                      />
+                      <div className="text-rc24 mt-rc20">Isabella Brandt</div>
+                      <div className="text-rc17">Events Manager EMEA</div>
+                      <div className="text-rc12">Linkedin</div>
+                    </div>
+                    <div className="text-white ml-rc90">
+                      <img
+                        src="https://o9solutions.com/wp-content/uploads/2021/06/Daria-Iatmanova.jpeg"
+                        alt=""
+                        width={258}
+                        height={258}
+                      />
+                      <div className="text-rc24 mt-rc20">Daria Iatmanova</div>
+                      <div className="text-rc17">Events Marketing EMEA</div>
+                      <div className="text-rc12">Linkedin</div>
+                    </div>
+                  </div>
                 </Col>
               </Row>
             </div>
